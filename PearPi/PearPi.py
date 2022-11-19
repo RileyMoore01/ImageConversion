a1db490c6b254fa9ab3e8c221b874548e41349e2
RileyMoore01 <50551379+RileyMoore01@users.noreply.github.com>
2022-11-19 16:58:23 -0600

    Update PearPi.py

--- PearPi/PearPi.py
+++ PearPi/PearPi.py
@@ -5,7 +5,6 @@ import sys
 
 from tkinter import *
 from time import sleep
-from time import sleep
 # from matplotlib.pyplot import imread
 from PIL import Image, ImageChops, ImageOps
 from gpiozero import LightSensor, Buzzer
@@ -22,7 +21,7 @@ NEWTIMEDELAY = 0
 BUTTON_HEIGHT = 150
 BUTTON_WIDTH = 150
 INDEX = 0
-
+BUZZER = Buzzer(17)
 
 ############################################################
 ##                      Main                              ##
@@ -31,32 +30,38 @@ INDEX = 0
 
 
 def CompareImages():
-    global INDEX, THRESHOLD
+    global INDEX, THRESHOLD, BUZZER
     INDEX = 0
-    thres = THRESHOLD % 10
-
-    img1 = Image.open("Images/ref.jpg")
-    img2 = Image.open("Images/pic"+str(INDEX)+".jpg")
+    
+    thres = int(THRESHOLD) / 10
+    thres = int(thres)
+    
+    if thres == 0:
+        thres = 1
+    
+    print(thres)
+    img1 = Image.open("/home/pearpi/Desktop/Images/ref.jpg")
+    img2 = Image.open("/home/pearpi/Desktop/Images/pic"+str(INDEX)+".jpg")
     img2 = img2.resize(img1.size)
 
     #Grayscale
-    img1 = img1.convert('L')
-    img2 = img2.convert('L')
+    #img1 = img1.convert('L')
+    #img2 = img2.convert('L')
 
     #Threshold
-    img1 = img1.point(lambda p: 255 if p > thres else 0)
-    img2 = img2.point(lambda p: 255 if p > thres else 0)
+#    img1 = img1.point(lambda p: 255 if p > 45 else 0)
+#    img2 = img2.point(lambda p: 255 if p > 45 else 0)
 
     #Monochrome
-    img1 = img1.convert('1')
-    img2 = img2.convert('1')
+#    img1 = img1.convert('1')
+#    img2 = img2.convert('1')
 
     diff = ImageChops.difference(img1, img2)
 
     if diff.getbbox():
-        # buzzer.on()
+        # BUZZER.on()
         sleep(1)
-        # buzzer.off()
+        # BUZZER.off()
         diff.show()
         #16x20
 
@@ -69,7 +74,9 @@ def captureImage():
 
 def main():
     global TIMEDELAY
-    # buzzer = Buzzer(17)
+    sleep(TIMEDELAY)
+    captureImage()
+    CompareImages()
 
     # Read in the sensors output
     sensor = False;
@@ -143,7 +150,7 @@ def clickExitButton():
 
 # Position button  ---------------------------------------------------------------------
 positionIcon = PhotoImage(file='/home/pearpi/Desktop/position.gif')
-positionIcon = positionIcon.subsample(2, 2)
+#positionIcon = positionIcon.subsample(2, 2)
 
 positionLabel = Label(image=positionIcon)
 positionLabel.config(font=('Helvatical bold', 10), bg='white')
@@ -155,7 +162,7 @@ positionButton.place(x=0,y=10)
 
 # Reference Image Button  ---------------------------------------------------------------
 referenceIcon = PhotoImage(file='/home/pearpi/Desktop/camera.gif')
-referenceIcon = referenceIcon.subsample(2, 2)
+#referenceIcon = referenceIcon.subsample(2, 2)
 
 referenceLabel = Label(image=referenceIcon)
 referenceLabel.config(font=('Helvatical bold', 5), bg='white')
@@ -174,11 +181,11 @@ runProgramLabel.config(font=('Helvatical bold', 5), bg='white')
 
 runButton = Button(win, image=runIcon,command= RunProgram,borderwidth=0, highlightthickness=0
     , height=BUTTON_HEIGHT, width=BUTTON_WIDTH, bg='white')
-runButton.place(x=880,y=370)
+runButton.place(x=800,y=300)
 
 # Exit program button  -------------------------------------------------------------------
 exitIcon = PhotoImage(file='/home/pearpi/Desktop/iconExit.gif')
-exitIcon = exitIcon.subsample(2, 2)
+#exitIcon = exitIcon.subsample(2, 2)
 
 exitLabel = Label(image=exitIcon)
 exitLabel.config(font=('Helvatical bold', 5), bg='white')
