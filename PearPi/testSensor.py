27a3dad8c01af668e0659b2db0de236a2caaec6b
RileyMoore01 <50551379+RileyMoore01@users.noreply.github.com>
2022-11-20 16:11:22 -0600

    Create testSensor.py

---
+++ PearPi/testSensor.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO  
+GPIO.setmode(GPIO.BCM)       # set up BCM GPIO numbering  
+GPIO.setup(25, GPIO.IN)      # set GPIO 25 as input  
+  
+if GPIO.input(25):           # if port 25 == 1  
+    print "Port 25 is 1/GPIO.HIGH/True"  
+else:  
+    print "Port 25 is 0/GPIO.LOW/False"  
+  
+GPIO.cleanup()    
