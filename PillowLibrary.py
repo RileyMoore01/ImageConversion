5de65652db0a05f5e464b4eeb452298b1eba6a7e
RileyMoore01 <xrileyxmoorex@outlook.com>
2022-11-20 13:41:34 -0600

    f

--- PillowLibrary.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageChops
-
-img1 = Image.open("IMG_5362.jpg")
-img2 = Image.open("IMG_5363.jpg")
-
-diff = ImageChops.difference(img1, img2)
-
-if diff.getbbox():
-    diff.show()
